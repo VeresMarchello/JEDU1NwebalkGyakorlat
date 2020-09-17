892fdaef24bc0352ee8ae9e70175da107bd784ab
Marcell Veres <marcell.veres@bayzoltan.hu>
2020-09-17 16:18:23 +0200

    initial commit

---
+++ VM_calculator/src/calculator/Main.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+
+	}
+
+	private int Add(int a, int b)
+	{
+		//returns a + b
+		return a + b;
+	}
+
+	private int Substract(int a, int b)
+	{
+		return a - b;
+	}
+}
