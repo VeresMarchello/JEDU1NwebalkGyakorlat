0023416bdc920c02f343484b9664d9d4fbe6f83c
Marcell Veres <marcell.veres@bayzoltan.hu>
2020-09-17 17:29:11 +0200

    Fix

--- VM_calculator/src/calculator/Substract.java
+++ VM_calculator/src/calculator/Substract.java
@@ -5,6 +5,7 @@ public class Substract
 	public static int SubstractMethod(int a, int b) 
 	{
 		// returns a-b
+		// conflict
 		return a - b;
 	} 
 }
--- VM_calculator/src/calculator/Sum.java
+++ VM_calculator/src/calculator/Sum.java
@@ -5,6 +5,7 @@ public class Sum
 	public static int SumMethod(int a, int b) 
 	{
 		// returns a+b
+		// fix
 		return a + b;
 	}
 }
